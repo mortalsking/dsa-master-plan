4dcf68a91eda22e2646627f4ded1b1c31dbc058b
mortalsking <rushikeshbardeskar28@gmail.com>
2026-03-01 17:08:01 +0530

    Add implementation for Two Pointer technique in Code Templates

---
+++ cp-dsa-practice/Code-Templates/TwoPointer.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fn(vector<int>& arr1, vector<int>& arr2) {
+    int i = 0, j = 0, ans = 0;
+
+    while (i < arr1.size() && j < arr2.size()) {
+        // do some logic here
+        if (arr1[i] < arr2[j]) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+
+    while (i < arr1.size()) {
+        // do logic
+        i++;
+    }
+
+    while (j < arr2.size()) {
+        // do logic
+        j++;
+    }
+
+    return ans;
+}
