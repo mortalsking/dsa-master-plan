20ab93395d3050caea6e2ebbeaeb6d80feb314e5
mortalsking <rushikeshbardeskar28@gmail.com>
2026-03-07 19:47:29 +0530

    Refactor BFS approach for Number of Islands problem to improve readability and maintainability

--- cp-dsa-practice/Concepts/Graphs/medium/Number Of Islands.cpp
+++ cp-dsa-practice/Concepts/Graphs/medium/Number Of Islands.cpp
@@ -44,48 +44,48 @@ private:
     }
 };
 //Approach: bfs to mark the visited land cells and count the number of islands
-// class Solution {
-// public:
-//     int numIslands(vector<vector<char>>& grid) {
-//         if (grid.empty() || grid[0].empty()) {
-//             return 0;
-//         }
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        if (grid.empty() || grid[0].empty()) {
+            return 0;
+        }
         
-//         int numIslands = 0;
-//         int m = grid.size();
-//         int n = grid[0].size();
-//         vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int numIslands = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         
-//         queue<pair<int, int>> q;
+        queue<pair<int, int>> q;
         
-//         for (int i = 0; i < m; i++) {
-//             for (int j = 0; j < n; j++) {
-//                 if (grid[i][j] == '1') {
-//                     numIslands++;
-//                     q.push({i, j});
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    numIslands++;
+                    q.push({i, j});
                     
-//                     while (!q.empty()) {
-//                         auto [x, y] = q.front();
-//                         q.pop();
+                    while (!q.empty()) {
+                        auto [x, y] = q.front();
+                        q.pop();
                         
-//                         if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] != '1') {
-//                             continue;
-//                         }
+                        if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] != '1') {
+                            continue;
+                        }
                         
-//                         grid[x][y] = '0'; // mark as visited
+                        grid[x][y] = '0'; // mark as visited
                         
-//                         for (auto& dir : directions) {
-//                             int nx = x + dir.first;
-//                             int ny = y + dir.second;
-//                             if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == '1') {
-//                                 q.push({nx, ny});
-//                             }
-//                         }
-//                     }
-//                 }
-//             }
-//         }
+                        for (auto& dir : directions) {
+                            int nx = x + dir.first;
+                            int ny = y + dir.second;
+                            if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == '1') {
+                                q.push({nx, ny});
+                            }
+                        }
+                    }
+                }
+            }
+        }
         
-//         return numIslands;
-//     }
-// };
+        return numIslands;
+    }
+};
