5077dd82dbdf7a05bd42b47fc76484d9b7af4e05
mortalsking <rushikeshbardeskar28@gmail.com>
2026-04-02 23:24:00 +0530

    Added solution of Maximum Amount of Money Robot can Earn

---
+++ cp-dsa-practice/Patterns/DynamicProgramming/medium/Maximum Amount of Money Robot Can Earn.cpp
@@ -0,0 +1,26 @@
+// Problem: https://leetcode.com/problems/maximum-amount-of-money-robot-can-earn/
+// Time Complexity: O(n*m) where n and m are the dimensions of the coins grid. We fill a 3D DP table of size n*m*3.
+// Space Complexity: O(n*m*3) for the DP table.
+//day 34 of 100 day challenge
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maximumAmount(vector<vector<int>>& coins) {
+        int n = coins.size(), m = coins[0].size();
+        vector dp(n, vector(m, vector<int>(3, -1e9)));
+        dp[0][0][1] = dp[0][0][2] = 0, dp[0][0][0] = coins[0][0];
+        
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                for (int k = 0; k < 3; k++) {
+                    if (i) dp[i][j][k] = max(dp[i][j][k], dp[i - 1][j][k] + coins[i][j]);
+                    if (i && k) dp[i][j][k] = max(dp[i][j][k], dp[i - 1][j][k - 1]);
+                    if (j) dp[i][j][k] = max(dp[i][j][k], dp[i][j - 1][k] + coins[i][j]);
+                    if (j && k) dp[i][j][k] = max(dp[i][j][k], dp[i][j - 1][k - 1]);
+                }
+        int ans = *max_element(dp[n - 1][m - 1].begin(), dp[n - 1][m - 1].end());
+        return ans;
+    }
+};
